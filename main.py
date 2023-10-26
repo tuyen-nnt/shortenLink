867d3aec0cb922ee3fc660780bd88cf5caa7643f
Tuyen Nguyen <tuyendev@gmail.com>
2023-10-26 21:13:37 +0700

    update user

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def make_app():
         host=os.getenv('DB_HOST', 'localhost'),
         # https://stackoverflow.com/questions/4906977/how-to-access-environment-variable-values
         port=int(os.getenv('DB_PORT', 3306)),
-        username=os.getenv('DB_USER', 'sqlalchemy'),
+        username=os.getenv('DB_USER', 'tuyentuyen'),
         password=os.getenv('DB_PASSWORD', 'Password@123'),
         db_name=os.getenv('DB_NAME', 'shortlink')
     )
